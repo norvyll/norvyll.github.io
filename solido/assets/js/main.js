34d048868fd0060787dc6d473dc84fce4977f59c
norvyll <88539171+norvyll@users.noreply.github.com>
2021-09-26 00:01:40 +0200

    Add files via upload

---
+++ solido/assets/js/main.js
@@ -0,0 +1,23 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    })
+});
+
+window.onscroll = function () { myFunction() };
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
